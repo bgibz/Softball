34fd64603fe07bc3539bd99dc5dfed157a7b4016
bgibz <brendanjamesgibson@gmail.com>
2019-06-04 00:05:03 -0700

    manipulate inning with buttons on scoreboard

--- src/Scoreboard.jsx
+++ src/Scoreboard.jsx
@@ -33,18 +33,45 @@ class Scoreboard extends React.Component {
             [name]: value
         });
     }
-    endhalf() {
+
+    endhalf(action) {
         if (this.state.half === "top") {
-            this.setState((state) => {
-                return { half: "bottom",
-            outs: 0}
-            });
+            if (action === "next") {
+                this.setState(() => {
+                    return { 
+                        half: "bottom",
+                        outs: 0
+                    }
+                });
+            }
+            if (action === "last" && this.state.inning > 1) {
+                this.setState((state) => {
+                    return {
+                        half: "top",
+                        inning: state.inning - 1,
+                        outs: 0
+                    }
+                });
+            }
         } else{
-            this.setState((state) => {
-                return { half: "top",
-                inning: state.inning + 1,
-                outs: 0 }
-            });
+            if (action === "next") {
+                this.setState((state) => {
+                    return { 
+                        half: "top",
+                        inning: state.inning + 1,
+                        outs: 0 
+                    }
+                });
+            }
+            if (action === "last") {
+                this.setState(() => {
+                    return { 
+                        half: "top",
+                        outs: 0 
+                    }
+                });
+            }
+            
         }
     }
 
@@ -118,7 +145,7 @@ class Scoreboard extends React.Component {
                             <tr>
                                 <th>{this.state.home}</th>
                                 <td>
-                                    <table className = "innerTable">                                
+                                    <table className = "hiddenTable">                                
                                         <tbody>
                                             <tr>
                                             <td rowSpan = '2'>{this.state.homescore}</td>
@@ -130,12 +157,16 @@ class Scoreboard extends React.Component {
                                         </tbody>
                                     </table>
                                 </td>
-                                <th rowSpan = '2'><span className={arrow} aria-hidden="true" style={{color: 'purple'}}></span>{this.state.inning}</th>
+                                <th rowSpan = '2'>
+                                    <span className={arrow} aria-hidden="true" style={{color: 'black'}}></span>{this.state.inning}
+                                    <button className='btn btn-secondary tableButton' onClick = {(e) => this.endhalf("last")}><span className = 'fa fa-caret-left'></span></button> 
+                                    <button className='btn btn-secondary tableButton' onClick = {(e) => this.endhalf("next")}><span className = 'fa fa-caret-right'></span></button>
+                                </th>
                             </tr>
                             <tr>
                                 <th>{this.state.away}</th>
                                 <td>
-                                    <table className = "innerTable">                                
+                                    <table className = "hiddenTable">                                
                                         <tbody>
                                             <tr>
                                             <td rowSpan = '2'>{this.state.awayscore}</td>
@@ -154,10 +185,13 @@ class Scoreboard extends React.Component {
                 <div className="col-md-12">
                     <p>Out:  <span dangerouslySetInnerHTML={{__html: outs}}></span></p>
                     <br></br>
-                    <button onClick={(e) => this.endhalf()} >End half inning </button>
-                    <br/>
-                    <button onClick={(e) => this.addOut()}>Out!</button>
-                    <br/>
+                    <table className = "hiddenTable">
+                        <tbody>
+                            <tr>
+                                <td><buttton className='btn btn-danger' onClick={(e) => this.addOut()}>Out!</buttton></td> 
+                            </tr>
+                        </tbody>
+                    </table>
                     <button onClick={(e) => this.props.nextBatter()}>Next Batter</button>
                 </div>
                 </div>
