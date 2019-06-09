b25f8eab589ffd922a98be45665a9828afc9c977
bgibz <brendanjamesgibson@gmail.com>
2019-06-08 21:54:32 -0700

    add Matt's song

--- src/walkupService.js
+++ src/walkupService.js
@@ -5,6 +5,7 @@ import tonySound from './media/Tony.mp3';
 import iainSound from './media/Iain.mp3';
 import kellySound from './media/Kelly.mp3';
 import seanSound from './media/Sean.mp3';
+import mattSound from './media/Matt.mp3';
 
 
 class walkupService {
@@ -33,6 +34,9 @@ class walkupService {
             src: [seanSound],
             html5: true
         });
+        var matt = new Howl ({
+            src: [mattSound]
+        })
         this.sounds = new Map();
         this.sounds.set('Alyssa', alyssa);
         this.sounds.set('Danica', danica);
@@ -40,8 +44,7 @@ class walkupService {
         this.sounds.set('Iain', iain);
         this.sounds.set('Kelly', kelly);
         this.sounds.set('Sean', sean);
-
-        //this.playWalkup = this.playWalkup.bind(this);
+        this.sounds.set('Matt', matt);
     }
 
     playWalkup(playerName){
@@ -55,11 +58,6 @@ class walkupService {
         }
         if (myHowl !== undefined){
             setTimeout(function() { 
-                for (var [key, value] of this.sounds) {
-                    if (key !== playerName){
-                        value.stop();
-                    }
-                }
                 myHowl.play();
                 myHowl.fade(1, 0, 30000);
             }, 3000);
