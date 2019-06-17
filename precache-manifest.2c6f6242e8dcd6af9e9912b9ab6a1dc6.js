209f97ce4652b2a32f7bb9f9348cdc6eaaedb50e
bgibz <brendanjamesgibson@gmail.com>
2019-06-16 23:31:17 -0700

    Updates

---
+++ precache-manifest.2c6f6242e8dcd6af9e9912b9ab6a1dc6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f2fcf7b9aded99b12c5f28b71d5247",
+    "url": "/softball/index.html"
+  },
+  {
+    "revision": "8365e4a0edbfd00e7189",
+    "url": "/softball/static/css/main.1fc24f48.chunk.css"
+  },
+  {
+    "revision": "1a102e08ef06429fabf1",
+    "url": "/softball/static/js/2.c34f57a1.chunk.js"
+  },
+  {
+    "revision": "8365e4a0edbfd00e7189",
+    "url": "/softball/static/js/main.1c2582db.chunk.js"
+  },
+  {
+    "revision": "0cd2594178c307b66aec",
+    "url": "/softball/static/js/runtime~main.2e4b207d.js"
+  },
+  {
+    "revision": "800c0fa45294cf2cca29ae2d37418d99",
+    "url": "/softball/static/media/Allie.800c0fa4.mp3"
+  },
+  {
+    "revision": "19531981859ab0b7f69d57034ea9ebee",
+    "url": "/softball/static/media/Allison.19531981.mp3"
+  },
+  {
+    "revision": "cc9d54e5843b482cc24f57668fc5a6bb",
+    "url": "/softball/static/media/Alyssa.cc9d54e5.mp3"
+  },
+  {
+    "revision": "52907b7df44b31b4b8ad7c8552eef075",
+    "url": "/softball/static/media/Brendan.52907b7d.mp3"
+  },
+  {
+    "revision": "59720df639affd898576e2a4dd0e16c5",
+    "url": "/softball/static/media/Dan.59720df6.mp3"
+  },
+  {
+    "revision": "a2b3a9fe2854b61782fddb7eb9049661",
+    "url": "/softball/static/media/Danica.a2b3a9fe.mp3"
+  },
+  {
+    "revision": "0f6b9a7c39f3f5bba5022d425819c1ef",
+    "url": "/softball/static/media/Emma.0f6b9a7c.mp3"
+  },
+  {
+    "revision": "516b6b1015280ad28a71aacbe0c3c68e",
+    "url": "/softball/static/media/Iain.516b6b10.mp3"
+  },
+  {
+    "revision": "455f80bb2bff7f4db0273e3a20d6f038",
+    "url": "/softball/static/media/Julia.455f80bb.mp3"
+  },
+  {
+    "revision": "bb692bdc6949bc7e9521a554d7e836f2",
+    "url": "/softball/static/media/Kelly.bb692bdc.mp3"
+  },
+  {
+    "revision": "9b263cc059df5818a849819887e3829b",
+    "url": "/softball/static/media/Kevin.9b263cc0.mp3"
+  },
+  {
+    "revision": "8f1ac0282eb5963dc216b758fb88fde7",
+    "url": "/softball/static/media/Matt.8f1ac028.mp3"
+  },
+  {
+    "revision": "a956024ca73edb4d112799c89c59b47b",
+    "url": "/softball/static/media/Mike.a956024c.mp3"
+  },
+  {
+    "revision": "6df940b1a26d322c94b7fd19c25ccb79",
+    "url": "/softball/static/media/Robbie.6df940b1.mp3"
+  },
+  {
+    "revision": "8d20928da95cfcbb468de2de8b21b31b",
+    "url": "/softball/static/media/Sarah.8d20928d.mp3"
+  },
+  {
+    "revision": "fc528c74fa6d78f71412c280b483adc5",
+    "url": "/softball/static/media/Sean.fc528c74.mp3"
+  },
+  {
+    "revision": "90f91bee82c41e69f6213aad7b6f1bf3",
+    "url": "/softball/static/media/Tony.90f91bee.mp3"
+  }
+]);
